c13dc656d74998ed35a2157de796c1e3614e9cbb
Ryan Cheung <rec324@cornell.edu>
2026-04-26 23:38:15 -0400

    add user and major requirement tables and update seed_courses.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,18 @@
 from flask import Flask
 
+from src.db import db
 from src.routes.routes import main
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///courses.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
     app.register_blueprint(main)
     return app
